101da16e67e4888770fe599429d4c08f37a16fb8
Ankit Gupta <ankit.gupta2801@gmail.com>
2019-03-23 12:54:05 -0700

    initial commit

---
+++ src/iat265/aga53/Scrubbable.java
@@ -0,0 +1,21 @@
+package iat265.aga53;
+
+import java.util.Iterator;
+
+public interface Scrubbable {
+    String getName();
+
+    void setName(String parameter);
+
+    String[] getProperties();
+
+    void setParameter(String parameter, float value);
+
+    float getParameter(String parameter);
+
+    void draw();
+
+    Scrubbable pick(int x, int y);
+
+    Iterator<Scrubbable> createIterator();
+}
